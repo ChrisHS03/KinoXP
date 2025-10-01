66e2b0d3fc9fdd4b8fbc30a276292158ab9167cf
daniellaisabella <dato0001@stud.kea.dk>
2025-10-01 11:44:51 +0200

    UPDATE movie + DELETE movie backend done

--- src/main/java/com/example/kinoxp/controller/MovieRestController.java
+++ src/main/java/com/example/kinoxp/controller/MovieRestController.java
@@ -4,9 +4,11 @@ import com.example.kinoxp.model.Movie;
 import com.example.kinoxp.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -26,4 +28,27 @@ public class MovieRestController {
         return movieRepository.save(movie);
     }
 
+    @PutMapping("/updatemovie/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable int id, @RequestBody Movie movie) {
+        Optional<Movie> optionalMovie = movieRepository.findById(id);
+        if (optionalMovie.isPresent()) {
+            movie.setMovie_id(optionalMovie.get().getMovie_id());
+            movieRepository.save(movie);
+            return ResponseEntity.ok(movie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/deletemovie/{id}")
+        public ResponseEntity<String> deleteMovie(@PathVariable int id){
+        Optional<Movie> optionalMovie = movieRepository.findById(id);
+        if(optionalMovie.isPresent()){
+            movieRepository.deleteById(id);
+            return ResponseEntity.ok("Movie deleted successfully");
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
+    }
 }
