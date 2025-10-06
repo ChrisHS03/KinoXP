e71e948cefd564445705c697b8a1fd31057a4f5c
Badevand <mu4maroc@gmail.com>
2025-10-06 13:24:54 +0200

    asd

---
+++ src/main/java/com/example/kinoxp/model/Booking.java
@@ -0,0 +1,67 @@
+
+
+package com.example.kinoxp.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer booking_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "show_id", referencedColumnName = "show_id", nullable = false)
+
+    @Column(nullable = false)
+    private LocalDateTime bookingTime;
+
+    @Column(nullable = false)
+    private int numberOfSeats;
+
+    public Booking() {}
+    public Booking(Integer booking_id, User user, LocalDateTime bookingTime, int numberOfSeats) {
+        this.booking_id = booking_id;
+        this.user = user;
+        this.bookingTime = bookingTime;
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public Integer getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(Integer booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+}
